61422fd0066591636ca1c6ba42d7c72305017edd
Jake Cerwin <jcerwin@andrew.cmu.edu>
2022-04-02 13:11:37 -0400

    InitialWorking

--- Django/Occupancy/Occupancy/settings.py
+++ Django/Occupancy/Occupancy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'q0pc8ax_f$@2=4_)wz!rgw50mef)z5jew40e4ppth$$%w5=@-='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
