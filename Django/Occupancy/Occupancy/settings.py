6bb10baf17f904a6b214d41d4489593f38ddcc8d
Jake Cerwin <jcerwin@andrew.cmu.edu>
2022-04-05 11:53:36 -0400

    remove os change

--- Django/Occupancy/Occupancy/settings.py
+++ Django/Occupancy/Occupancy/settings.py
@@ -118,8 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
