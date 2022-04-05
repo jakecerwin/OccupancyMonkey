ff8c05b16c480d9f6fead42221345607306e9bca
Jake Cerwin <jcerwin@andrew.cmu.edu>
2022-04-05 12:01:59 -0400

    try this

--- Django/Occupancy/Occupancy/wsgi.py
+++ Django/Occupancy/Occupancy/wsgi.py
@@ -11,10 +11,12 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/ubuntu/OccupancyMonkey/Django/Occupancy/Occupancy')
+sys.path.append('/home/ubuntu/OccupancyMonkey/Django/Occupancy')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Occupancy.settings')
 
 application = get_wsgi_application()
 
 
-sys.path.append('/home/ubuntu/OccupancyMonkey/Django/Occupancy/Occupancy')
-sys.path.append('/home/ubuntu/OccupancyMonkey/Django/Occupancy')
+
