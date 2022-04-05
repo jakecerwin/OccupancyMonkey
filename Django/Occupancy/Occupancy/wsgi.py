7a19e5cc3116cb27d12d3e94cfdaafb6950918c0
Jake Cerwin <jcerwin@andrew.cmu.edu>
2022-04-05 11:59:30 -0400

    wsgi attempt fix

--- Django/Occupancy/Occupancy/wsgi.py
+++ Django/Occupancy/Occupancy/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Occupancy.settings')
 
 application = get_wsgi_application()
+
+
+sys.path.append('/home/ubuntu/OccupancyMonkey/Occupancy/Occupancy')
+sys.path.append('/home/ubuntu/OccupancyMonkey/Occupancy')
