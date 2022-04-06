16582d39bd8a727303345b61ccc8235a8557d25a
Jake Cerwin <jcerwin@andrew.cmu.edu>
2022-04-06 04:56:20 -0400

    try without string

--- Django/Occupancy/mapping/views.py
+++ Django/Occupancy/mapping/views.py
@@ -48,7 +48,7 @@ def get_status(request):
 @csrf_exempt
 def data(request):
   print("entering data")
-  data = json.loads(str(request.read()))['table_id']
+  data = json.loads(request.read())['table_id']
   
 
 
