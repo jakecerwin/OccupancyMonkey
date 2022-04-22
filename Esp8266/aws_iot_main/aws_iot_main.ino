c690d78b5e4c3f858b34e2eb72bbb30f937beb34
Ryan <huangryand@gmail.com>
2022-04-21 21:04:00 -0400

    Thermal is working!
    Wrote code to actually read from thermal, convert thermal reads to a
    bool, and rewrote detection code to use PIR and thermal bools to
    determine occupancy. Thresholds might want to be adjusted but so far
    everything works.

--- Esp8266/aws_iot_main/aws_iot_main.ino
+++ Esp8266/aws_iot_main/aws_iot_main.ino
@@ -1,3 +1,35 @@
+/***********************************
+Occupancy Monkey (CMU ECE Capstone Project)
+Authors: Jake Cerwin, Ryan Huang
+Sections of Omron thermal sensor code were taken from the official Omron
+devhub Github page: https://github.com/omron-devhub/d6t-2jcieev01-arduino.
+These sections are indicated in the code. In addition, the following license
+is provided:
+
+MIT License
+Copyright (c) 2019, 2018 - present OMRON Corporation
+All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+***********************************/
+
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
@@ -9,20 +41,28 @@
 
 #define D6T_addr 0x0A
 #define D6T_cmd 0x4C
-#define buflen 16
-
-int PIRIn = D7;
-int SDAIn = D4;
-int SCLIn = D5;
-
+#define numPixels 16
+#define numReads 35 //two reads per pixel to get a double, plus 3 extraneous ones
+#define occupancyTemp 20.5 //room temp is around 20 degC
+#define noiseTemp 35 //body temperature is around 32 degC, so if we're above 35 it's probably noise
+#define unoccupiedLimit 10 //number of consecutive unoccupied readings to change status to unoccupied
+#define occupiedLimit 10 //number of consecutive occupied readings to change status to occupied
 
 String Status = "unoccupied";
 String oldstatus = "unoccupied";
-float h ;
+float h;
 float t;
 unsigned long lastMillis = 0;
 unsigned long previousMillis = 0;
 const long interval = 5000;
+int occupiedCount = 0;
+int unoccupiedCount = 0;
+
+uint8_t ptatBuf[numReads];
+double tempBuf[numPixels];
+int pirRead;
+int pirIn = D7;
+bool thermalRead;
 
 #define AWS_IOT_PUBLISH_TOPIC   "esp8266_10E_03/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp8266_10E_03/sub"
@@ -126,44 +166,117 @@ void publishMessage()
   client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
 }
 
-//Dummy update for now
-
-uint8_t calc_crc(uint8_t data) {
-    int index;
-    uint8_t temp;
-    for (index = 0; index < 8; index++) {
-        temp = data;
-        data <<= 1;
-        if (temp & 0x80) {data ^= 0x07;}
-    }
-    return data;
+//Converts an 8-bit unsigned to a 16-bit signed -- modified from Omron Github repo
+int16_t conv8us_s16_le(int n) {
+    int ret;
+    ret = ptatBuf[n];
+    ret += ptatBuf[n + 1] << 8;
+    return (int16_t)ret;
 }
 
-/** <!-- D6T_checkPEC {{{ 1--> D6T PEC(Packet Error Check) calculation.
- * calculate the data sequence,
- * from an I2C Read client address (8bit) to thermal data end.
- */
-bool D6T_checkPEC(uint8_t buf[], int n) {
-    int i;
-    uint8_t crc = calc_crc((D6T_addr << 1) | 1);  // I2C Read address (8bit)
-    for (i = 0; i < n; i++) {
-        crc = calc_crc(buf[i] ^ crc);
+void getThermalReadings()
+{
+  memset(ptatBuf, 0, numReads);
+  memset(tempBuf, 0, numPixels);
+  
+  //Let thermal sensor know we're about to request
+  Wire.beginTransmission(D6T_addr);
+  Wire.write(D6T_cmd);
+  Wire.endTransmission();
+
+  //Ask for all data points from sensor
+  Wire.requestFrom(D6T_addr, numReads);
+
+  //Readings come in PTAT (Proportional To Absolute Temperature) form
+  int i=0;
+  while (Wire.available()) {
+    uint8_t reading = Wire.read();
+    ptatBuf[i] = reading;
+    i++;
+  }
+
+  //Get reading in double form and convert from PTAT->temp in degC (from Omron Github repo)
+  double ptat = (double)conv8us_s16_le(0) / 10.0; //Filters out first 2 data points which aren't needed
+  for (i = 0; i < numPixels; i++) {
+    int16_t temp = conv8us_s16_le(2 + 2*i);
+    tempBuf[i] = (double)temp / 10.0;
+  }
+
+/*
+  //Output results to serial
+  Serial.print("PTAT:");
+  Serial.print(ptat, 1);
+  Serial.print(" [degC], Temperature: ");
+  for (i = 0; i < numPixels; i++) {
+    Serial.print(tempBuf[i], 1);
+    Serial.print(", ");
+  } 
+  Serial.println(" [degC]");
+*/
+}
+
+void detectOccupancy()
+{
+  //Take average of all thermal pixels for a thermal value
+  double avgTemp = 0;
+  for (int i = 0; i < numPixels; i++) {
+    avgTemp += tempBuf[i];
+  }
+  avgTemp /= numPixels;
+  //Serial.println("Average temp: ");
+  //Serial.println(avgTemp);
+
+  //Compare average temp to occupancy and noise thresholds
+  thermalRead = false;
+  if (avgTemp >= occupancyTemp && occupancyTemp <= noiseTemp)
+    thermalRead = true;
+
+  Serial.print("Thermal: ");
+  Serial.print(thermalRead);
+
+  //Case by current status -- occupied
+  if (Status == "occupied") {
+
+    //When occupied we're only using thermal readings to confirm occupancy
+    if (thermalRead)
+      unoccupiedCount = 0;
+
+    //Thermal sensor indicates no occupancy
+    else {
+      unoccupiedCount++;
+      if (unoccupiedCount >= unoccupiedLimit) {
+        oldstatus = Status;
+        Status = "unoccupied";
+        unoccupiedCount = 0;
+      }
     }
-    bool ret = crc != buf[n];
-    if (ret) {
-        Serial.print("PEC check failed:");
-        Serial.print(crc, HEX);
-        Serial.print("(cal) vs ");
-        Serial.print(buf[n], HEX);
-        Serial.println("(get)");
+  }
+
+  //Case by current status -- unoccupied
+  else {
+
+    //When unoccupied, we need a PIR read too -- this one is a bool with HIGH as true
+    pirRead = digitalRead(pirIn);
+
+    Serial.print(", PIR: ");
+    Serial.print(pirRead);
+
+    //Both thermal and PIR sensors indicate occupancy
+    if (thermalRead && pirRead == HIGH) {
+      oldstatus = Status;
+      Status = "occupied";
     }
-    return ret;
+  }
+
+  Serial.print(", Status: ");
+  Serial.println(Status);
 }
 
 void setup()
 {
-  pinMode(PIRIn, INPUT);
-  
+  pinMode(pirIn, INPUT);
+
+  //Initialize I2C connection
   Wire.begin();
   Wire.beginTransmission(D6T_addr);  // I2C client address
   Wire.write(0x02);                  // D6T register
@@ -204,7 +317,6 @@ void setup()
 
 int readDelay = 1000;
 int mintimeBetweenSendingMessages = 5000;
-uint8_t buf[buflen];
 int count = 0 ;
 
 void loop()
@@ -212,52 +324,10 @@ void loop()
 
   delay(readDelay);
 
-  memset(buf, 0, buflen);
-  Wire.beginTransmission(D6T_addr);
-  Wire.write(D6T_cmd);
-  Wire.endTransmission();
-
-  Serial.println("New read beginning");
-
-  // Read
-  Wire.requestFrom(D6T_addr, buflen);
-
-  int i=0;
-  while (Wire.available()) {
-    Serial.print("Value:");
-    Serial.println(i);
-    Serial.println(Wire.read());
-    //Serial.print(buf[i] + ",");
-    //Serial.println("");
-    //rbuf[i] = Wire.read();
-    i++;
-    }
-
-  Serial.println("");
-
-  if (D6T_checkPEC(buf, buflen - 1)) {
-      return;
-    }
+  getThermalReadings();
+  detectOccupancy();
 
 /*
-  int val = digitalRead(PIRIn);
-  if (val == HIGH) {
-    oldstatus = Status;
-    Status = "occupied";
-
-    count = 10;
-
-  } else {
-    if (count > 0) {
-      count = count - 1;
-    } else {
-      oldstatus = Status;
-      Status = "unoccupied";
-    }
-    readDelay = 1000;
-  }
-
-
   if (oldstatus != Status) {
     if (!client.connected())
     {
