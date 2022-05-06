1023cb8ff945f4058d3ea5f05da2ae3e8c2ebf19
Angela Zhang <ayzhang@andrew.cmu.edu>
2022-05-06 02:28:14 -0400

    changed home to demo

--- Django/Occupancy/mapping/urls.py
+++ Django/Occupancy/mapping/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from mapping import views
 
 urlpatterns = [
-    path('', views.home, name='mapping-home'),
+    path('', views.demo, name='mapping-demo'),
     path('lab/', views.lab, name='mapping-lab'),
     path('uc2/', views.uc, name='mapping-uc2'),
-    path('demo/', views.demo, name='mapping-demo'),
     path('get-status', views.get_status, name="get-status"),
     path('data', views.data, name="data"),
     path('receive', views.data, name="receive"),
